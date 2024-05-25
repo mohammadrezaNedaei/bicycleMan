cb568ca05e01cdce956b8c495ea2f5c263e8424d
mmdreza <mrezanedaee@gmail.com>
2024-05-25 22:53:40 +0330

    add some comments

--- bicycleMan/bicycleMan.cpp
+++ bicycleMan/bicycleMan.cpp
@@ -29,6 +29,7 @@ int main()
 	edge w[20][20];
 	bestPrice d[20];
 	city p[20];
+	//read file "in.txt"
 	ifstream in("in.txt");
 	in >> counter;
 	string line;
@@ -74,16 +75,22 @@ int main()
 			counter++;
 		}
 	}
+	//read file complite
+
+	//put max data in totalValue
 	for (int i = 0; i <= 12; i++)
 		for (int j = 0; j <= 12; j++)
 			if (w[i][j].totalValue == 0)
 				w[i][j].totalValue = 100000000;
 
+	//put first values in d array 
+	//first data is edges from 0 as start node ex) 0 to 1
 	for (int i = 0; i <= 12; i++)
 	{
 		d[i].price = w[0][i].totalValue;
 		p[i].cityNumber = 0;
 	}
+	//calculate data as minimum price of travel using DP
 	for (int k = 1; k <= counter; k++)
 		for (int i = 0; i <= 12; i++)
 			for (int j = 0; j <= 12; j++)
@@ -96,6 +103,7 @@ int main()
 				}
 	int mini = 100000000;
 	int miniMap = 0;
+	//find minimum from the end nodes in path
 	for (int i = 0; i < lastStop; i++)
 	{
 		if (n - i < 0)
@@ -108,6 +116,7 @@ int main()
 			miniMap = n - i;
 		}
 	}
+	//print path and the number of chosen hotel 
 	cout << d[miniMap].price << endl << 0 << endl;
 	path(p, miniMap);
 	cout << miniMap << " " << d[miniMap].hotelNumber << endl;
@@ -115,6 +124,7 @@ int main()
 
 void path(city p[], int i)
 {
+	//goes throw p array for the path
 	if (p[i].cityNumber != 0)
 	{
 		path(p, p[i].cityNumber);
